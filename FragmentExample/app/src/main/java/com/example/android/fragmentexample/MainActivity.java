36c9fca836dea47d7e2283798bca9b0981096123
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2019-04-03 19:57:35 +0500

    Add Fragments in MainActivity.java, create an object of your Fragment class, get a reference to the FragmentManager, begin a transaction to add the Fragment, and finally commit the FragmentTransaction.

---
+++ FragmentExample/app/src/main/java/com/example/android/fragmentexample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.fragmentexample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyFragment frag = new MyFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.my_layout, frag, "MyFragment");
+        transaction.commit();
+
+    }
+}
