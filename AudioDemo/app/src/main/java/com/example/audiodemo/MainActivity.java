483cf4616d7c6fcbaf5951920d1f1a29472de7ff
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2019-05-09 17:09:17 +0500

    AudioManager used to control audio stream.

--- AudioDemo/app/src/main/java/com/example/audiodemo/MainActivity.java
+++ AudioDemo/app/src/main/java/com/example/audiodemo/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.audiodemo;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
     private Button mPlay;
     private Button mPause;
 
@@ -17,9 +21,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMediaPlayer = MediaPlayer.create(this, R.raw.laugh);
         mPlay = findViewById(R.id.button);
         mPause = findViewById(R.id.button2);
+
+        mMediaPlayer = MediaPlayer.create(this, R.raw.laugh);
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        SeekBar volumeControl = findViewById(R.id.seekBar);
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(curVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
         mPlay.setOnClickListener(new View.OnClickListener() {
             @Override
